4d886dd590a94127159dc7a9b4627cda5f52ee93
torik <riqibrekele@gmail.com>
2022-01-22 18:50:11 +0700

    add components to container & fixes

--- components/Container.jsx
+++ components/Container.jsx
@@ -1,20 +1,77 @@
-import Block from "./Block"
+// import Block from "./Block"
 
-import blockData from "./BlockData"
+// import blockData from "./BlockData"
+
+import HtmlcssProject from '../components/htmlcss/HtmlcssProject'
+import htmlcssData from '../components/htmlcss/htmlcssData'
+
+import ReactNextProject from '../components/reactnext/ReactNextProject'
+import reactnextData from "./reactnext/reactnextData"
 
 const Container = () => {
     return (
         <div className="container">
-            {blockData.map((item, index) => (
-                <Block
-                    name={item.name}
-                    code={item.code}
-                    web_link={item.web_link}
-                    desc={item.desc}
-                    tech={item.tech}
-                    key={item.name + index}
-                />
-            ))}
+
+            <div className="container-card">
+                <div className="container-htmlcss">
+                    <h2>TEXT</h2>
+                    <p>TEXT</p>
+                </div>
+
+                <div className="container-content">
+                    <div className="container-card_heading">
+                        <h3>This Projects only use HTML & CSS</h3>
+                    </div>
+                    <div className="container-card_content">
+                        <div className="container-card_content-item">
+                            {htmlcssData.map((item, index) => (
+                                <HtmlcssProject
+                                name={item.name}
+                                code={item.code}
+                                web_link={item.web_link}
+                                desc={item.desc}
+                                tech={item.tech}
+                                key={item.name + index}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+    
+            <div className="container-divider">
+            ==================    DIVIDER    ==================
+            </div>
+
+            <div className="container-card">
+                <div className="container-reactnext">
+                    <h2>TEXT</h2>
+                    <p>TEXT</p>
+                </div>
+
+                <div className="container-content">
+                    <div className="container-card_heading">
+                        <h3>This Projects only use React.js or Next.js</h3>
+                    </div>
+                    <div className="container-card_content">
+                        <div className="container-card_content-item">
+                            {reactnextData.map((item, index) => (
+                                <ReactNextProject
+                                name={item.name}
+                                code={item.code}
+                                web_link={item.web_link}
+                                desc={item.desc}
+                                tech={item.tech}
+                                key={item.name + index}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+
         </div>
     )
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -4,9 +4,9 @@ const Header = () => {
     return (
         <div className="header-container">
             <div className="header-logo">
-                <h3>logo github</h3>
+                <h3>logo</h3>
                 <a href={link_code}>
-                    <h4>torik135</h4>
+                    <h4>github.com/torik135</h4>
                 </a>
             </div>
             
