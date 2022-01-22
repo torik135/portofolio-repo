3917d577a0fe7a146971f453f5faadbffc31e332
torik <riqibrekele@gmail.com>
2022-01-22 18:49:48 +0700

    add css files & import

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import '../styles/block.css'
 import '../styles/container.css'
 import '../styles/globals.css'
 import '../styles/header.css'
+import '../styles/htmlcss.css'
+import '../styles/reactnext.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
