4df299a6499b04db4cb993c7ed43e572ae22c43a
torik <riqibrekele@gmail.com>
2022-01-23 20:14:56 +0700

    add card.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
-import '../styles/block.css'
 import '../styles/container.css'
 import '../styles/globals.css'
 import '../styles/header.css'
-import '../styles/htmlcss.css'
-import '../styles/reactnext.css'
+import '../styles/projects.css'
+
+// card teomplate
+import '../styles/cardcss/card.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
